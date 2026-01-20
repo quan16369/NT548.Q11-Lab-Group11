9d78d16cce3b6eec09d7b6fde94489814d864a5a
Phan Huu Phuoc Loc <109939299+locphp@users.noreply.github.com>
2026-01-20 14:33:13 +0700

    test github action v3

--- variable.tf
+++ variable.tf
@@ -1,25 +1,25 @@
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
-  default     = "10.0.0.0/16"  
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidr" {
   description = "CIDR block for the public subnet"
   type        = string
-  default     = "10.0.1.0/24" 
+  default     = "10.0.1.0/24"
 }
 
 variable "private_subnet_cidr" {
   description = "CIDR block for the private subnet"
   type        = string
-  default     = "10.0.2.0/24" 
+  default     = "10.0.2.0/24"
 }
 
 variable "availability_zone" {
   description = "The availability zone for the subnets"
   type        = string
-  default     = "us-east-1"  
+  default     = "us-east-1"
 }
 
 variable "instance_type" {
@@ -43,5 +43,5 @@ variable "ami_id" {
 variable "allowed_ip" {
   description = "Allowed IP address"
   type        = string
-  default = "0.0.0.0/0"
+  default     = "0.0.0.0/0"
 }
